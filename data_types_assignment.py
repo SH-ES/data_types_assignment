8b8d027407658e0915bdba6df1aa9dc7ec267a36
SH-ES <shahina.hussains@gmail.com>
2025-05-14 18:04:47 +0500

    Add files via upload

---
+++ data_types_assignment.py
@@ -0,0 +1,35 @@
+#INTEGER (Whole number)
+number_of_shoes=10 #Variable and its value
+shoes_given_in_charity=3 #Variable and its value
+remaining_shoes=number_of_shoes-shoes_given_in_charity #Variable and its values with subtraction sign.
+print("Now I have",number_of_shoes-shoes_given_in_charity, "shoes.") #To print the string, the required text is surrounded with quotations.
+#Each string and variable is separated by comma.
+
+
+#STRING
+sentence1="Milk is a complete diet." #Variable and its value
+sentence2="Everyone should drink milk daily." #Variable and its value
+print(sentence1, sentence2) #To print the string, each variable is separated by comma, without quotations.
+
+diameter=10.5 #Variable and its value
+radius=diameter/2 #Variable and its value
+print(radius) #To print the float, variable is not separated by comma, because there is only one variable.
+
+
+#FLOAT (Number in decimal)
+circumsference=20.7 #Variable and its value
+π=3.14 #Variable and its value. This variable is universally accepted so it can not be changed. Value is float.
+double_of_π=2*π #Variable and its value
+radius=(circumsference/double_of_π)
+print(radius) #To print the float, variable is not separated by comma, because there is only one variable.
+
+circumsference_of_earth=40075 #Variable and its integer value
+radius_of_earth=circumsference_of_earth/double_of_π #Variable and its value using formula.
+number_of_earth=1 #Variable and its integer value
+print(number_of_earth) #To print the integer, variable is not separated by comma, because there is only one variable.
+print(radius_of_earth) #To print the float, variable is not separated by comma, because there is only one variable.
+print("The radius of eath is", radius_of_earth, ".")
+print("The radius of eath is", radius_of_earth, "and the circumference of earth is", circumsference_of_earth,".")
+print("There is", number_of_earth, "earth in our solar system.", "Its cicumsference is",
+circumsference_of_earth, "and its radius is", radius_of_earth, ".") #To print the string, the required text is surrounded with quotations.
+#Each string and variable is separated by comma.
