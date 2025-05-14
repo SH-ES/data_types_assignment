bad6f06409b8758a67a50ca9216031c405e28d7c
SH-ES <shahina.hussains@gmail.com>
2025-05-14 13:12:31 +0500

    Add files via upload

---
+++ arithmetic_assignment.py
@@ -0,0 +1,59 @@
+# TASK 1:Addition
+X=7
+Y=5
+sum=(X+Y)
+print(sum)
+
+# TASK 2:Subtraction
+remaining=(X-Y)
+print(remaining)
+
+# TASK 3:Multiplication
+product=(X*Y)
+print(product)
+
+# TASK 4:Division
+result=(X/Y)
+print(result)
+
+# TASK 5:Floor Division
+answer=(X//Y)
+print(answer)
+
+# TASK 1:Addition
+print(f"{X}+{Y}={sum}")
+
+# TASK 2:Subtraction
+print(f"{X}-{Y}={remaining}")
+
+# TASK 3:Multiplication
+print(f"{X}*{Y}={product}")
+
+# TASK 4:Division
+print(f"{X}/{Y}={result}")
+
+# TASK 5:Floor Division
+print(f"{X}//{Y}={answer}")
+
+# TASK 6:Modulus
+remainder=(X%Y)
+print(f"{X}%{Y}={remainder}")
+
+# TASK 7:Exponentiation
+Exponentiation=(X**Y)
+print(f"{X}**{Y}={Exponentiation}")
+
+# TASK 8:Bonus Challenge
+print(f"{X}+{Y}={sum}")
+print(f"{X}-{Y}={remaining}")
+print(f"{X}*{Y}={product}")
+print(f"{X}/{Y}={result}")
+print(f"{X}//{Y}={answer}")
+print(f"{X}%{Y}={remainder}")
+print(f"{X}**{Y}={Exponentiation}")
+
+print(f"{X}+{Y}={sum}, {X}-{Y}={remaining}, {X}*{Y}={product}, {X}/{Y}={result}, {X}//{Y}={answer}, {X}%{Y}={remainder}, {X}**{Y}={Exponentiation}")
+
+Print a neatly formatted table like this using f-strings: 
+ 
+print(f"7+5={7+5}") #I tried it from the Google, but could not understand.
